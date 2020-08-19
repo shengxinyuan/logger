20616c89a6f33c7d80d12a06235b99b063e12ac8
shengxinyuan <shengxinyuan@yuewen.com>
2020-08-19 15:58:47 +0800

    fix 打包问题

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -46,16 +46,6 @@ let mainConfig = {
   target: 'electron-main'
 }
 
-/**
- * Adjust mainConfig for development settings
- */
-if (process.env.NODE_ENV !== 'production') {
-  mainConfig.plugins.push(
-    new webpack.DefinePlugin({
-      '__static': `"${path.join(__dirname, '../static').replace(/\\/g, '\\\\')}"`
-    })
-  )
-}
 
 /**
  * Adjust mainConfig for production settings
--- .electron-vue/webpack.web.config.js
+++ .electron-vue/webpack.web.config.js
@@ -121,13 +121,6 @@ if (process.env.NODE_ENV === 'production') {
 
   webConfig.plugins.push(
     new MinifyPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, '../static'),
-        to: path.join(__dirname, '../dist/web/static'),
-        ignore: ['.*']
-      }
-    ]),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
